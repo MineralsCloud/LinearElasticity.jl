54f819d2e84e0e85f9c947a9ad0ad4d7b8d4a5f3
Qi Zhang <singularitti@outlook.com>
2020-02-25 05:16:34 -0500

    Implement src/Moduli.jl using type parameters

--- src/Moduli.jl
+++ src/Moduli.jl
@@ -1,29 +1,41 @@
 module Moduli
 
+export BulkModulus,
+    YoungModulus,
+    Lame1stParameter,
+    ShearModulus,
+    PoissonRatio,
+    LongitudinalModulus
+
 abstract type ElasticModulus{T} end
-struct BulkModulus{T}
-    v::T
-end
-struct YoungModulus{T}
-    v::T
-end
-struct Lame1stParameter{T}
-    v::T
-end
-struct ShearModulus{T}
-    v::T
-end
-struct PoissonRatio{T}
-    v::T
-end
-struct LongitudinalModulus{T}
-    v::T
-end
+struct BulkModulus{T} <: ElasticModulus{T} end
+struct YoungModulus{T} <: ElasticModulus{T} end
+struct Lame1stParameter{T} <: ElasticModulus{T} end
+struct ShearModulus{T} <: ElasticModulus{T} end
+struct PoissonRatio{T} <: ElasticModulus{T} end
+struct LongitudinalModulus{T} <: ElasticModulus{T} end
 
 # These are helper functions and should not be exported!
-_auxiliaryR(E::YoungModulus, λ::Lame1stParameter) = sqrt(E.v^2 + 9 * λ.v^2 + 2E.v * λ.v)
+_auxiliaryR(E, λ) = sqrt(E^2 + 9 * λ^2 + 2E * λ)
+_auxiliaryS(E, M) = sqrt(E^2 + 9 * M^2 - 10E * M)
 
-(::BulkModulus)(E::YoungModulus, λ::Lame1stParameter) = (E + 3λ + _auxiliaryR(E, λ)) / 6
-(::BulkModulus)(E::YoungModulus, G::ShearModulus) = E * G / (3 * (3G - E))
+BulkModulus(::YoungModulus{E}, ::Lame1stParameter{λ}) where {E,λ} =
+    BulkModulus((E + 3λ + _auxiliaryR(E, λ)) / 6)
+BulkModulus(::YoungModulus{E}, ::ShearModulus{G}) where {E,G} =
+    BulkModulus(E * G / (3 * (3G - E)))
+
+for T in (
+    :BulkModulus,
+    :YoungModulus,
+    :Lame1stParameter,
+    :ShearModulus,
+    :PoissonRatio,
+    :LongitudinalModulus,
+)
+    eval(quote
+        $T(v) = $T{v}()
+        (::$T{v})() where {v} = v
+    end)
+end
 
 end # module Moduli
