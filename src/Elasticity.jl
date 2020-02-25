ca33d05a3cd3ea5e4f02acea5e14777f143195be
Qi Zhang <singularitti@outlook.com>
2020-02-24 20:40:01 -0500

    Add `Engineering*` & corresponding methods

--- src/Elasticity.jl
+++ src/Elasticity.jl
@@ -2,7 +2,14 @@ module Elasticity
 
 using Tensors: SymmetricTensor
 
-export TensorStress, TensorStrain, TensorStiffness, TensorCompliance
+export TensorStress,
+    TensorStrain,
+    TensorStiffness,
+    TensorCompliance,
+    EngineeringStress,
+    EngineeringStrain,
+    EngineeringCompliance,
+    EngineeringStiffness
 
 struct TensorStress{T} <: AbstractMatrix{T}
     data::SymmetricTensor{2,3,T}
@@ -20,13 +27,39 @@ struct TensorCompliance{T} <: AbstractArray{T,4}
     data::SymmetricTensor{4,3,T}
 end
 
+struct EngineeringStress{T} <: AbstractVector{T}
+    data::NTuple{6,T}
+end
+
+struct EngineeringStrain{T} <: AbstractVector{T}
+    data::NTuple{6,T}
+end
+
+struct EngineeringStiffness{T} <: AbstractMatrix{T}
+    data::SymmetricTensor{2,6,T}
+end
+
+struct EngineeringCompliance{T} <: AbstractMatrix{T}
+    data::SymmetricTensor{2,6,T}
+end
+
+const Stress = Union{TensorStress,EngineeringStress}
+const Strain = Union{TensorStrain,EngineeringStrain}
+const Stiffness = Union{TensorStiffness,EngineeringStiffness}
+const Compliance = Union{TensorCompliance,EngineeringCompliance}
+
 Base.size(::Union{TensorStress,TensorStrain}) = (3, 3)
 Base.size(::Union{TensorStiffness,TensorCompliance}) = (3, 3, 3, 3)
+Base.size(::Union{EngineeringStress,EngineeringStrain}) = (6,)
+Base.size(::Union{EngineeringStiffness,EngineeringCompliance}) = (6, 6)
 
-Base.getindex(
-    A::Union{TensorStress,TensorStrain,TensorStiffness,TensorCompliance},
-    I::Vararg{Int},
-) = getindex(A.data, I...)
+Base.getindex(A::Union{Stress,Strain,Stiffness,Compliance}, I::Vararg{Int}) =
+    getindex(A.data, I...)
+
+Base.inv(c::TensorStiffness) = TensorCompliance(inv(c))
+Base.inv(s::TensorCompliance) = TensorStiffness(inv(s))
+Base.inv(c::EngineeringStiffness) = EngineeringCompliance(inv(c))
+Base.inv(s::EngineeringCompliance) = EngineeringStiffness(inv(s))
 
 for T in (:TensorStress, :TensorStrain)
     eval(quote
