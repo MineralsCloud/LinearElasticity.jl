378eae4d7e417bbf06991fc6ba029aa1815f6392
Qi Zhang <singularitti@outlook.com>
2020-02-24 19:38:51 -0500

    Add `TensorStress` & `TensorStrain`

--- src/Elasticity.jl
+++ src/Elasticity.jl
@@ -1,5 +1,22 @@
 module Elasticity
 
-# Write your package code here.
+using Tensors: SymmetricTensor
+
+export TensorStress, TensorStrain
+
+struct TensorStress{T}
+    data::SymmetricTensor{2,3,T}
+end
+
+struct TensorStrain{T}
+    data::SymmetricTensor{2,3,T}
+end
+
+for T in (:TensorStress, :TensorStrain)
+    eval(quote
+        $T(v::Union{AbstractVector,NTuple{6}}) = $T(SymmetricTensor{2,3}(v))
+        $T(xx, xy, xz, yy, yz, zz) = $T((xx, xy, xz, yy, yz, zz))
+    end)
+end
 
 end
