90f63b8d58597d008585898e51d9932eee93f619
Qi Zhang <singularitti@outlook.com>
2020-02-25 19:58:02 -0500

    Add methods for `YoungModulus`

--- src/Moduli.jl
+++ src/Moduli.jl
@@ -23,11 +23,6 @@ struct LongitudinalModulus{T} <: ElasticModulus{T}
     v::T
 end
 
-# These are helper functions and should not be exported!
-_auxiliaryR(E::YoungModulus, λ::Lame1stParameter) = sqrt(E.v^2 + 9 * λ.v^2 + 2E.v * λ.v)
-_auxiliaryS(E::YoungModulus, M::LongitudinalModulus) = sqrt(E.v^2 + 9 * M.v^2 - 10E.v * M.v)
-
-BulkModulus(K::BulkModulus, ::ElasticModulus) = K
 BulkModulus(E::YoungModulus, λ::Lame1stParameter) = BulkModulus((E.v + 3λ.v + _auxiliaryR(E, λ)) / 6)
 BulkModulus(E::YoungModulus, G::ShearModulus) = BulkModulus(E.v * G.v / (3 * (3G.v - E.v)))
 BulkModulus(E::YoungModulus, ν::PoissonRatio) = BulkModulus(E.v / (3 * (1 - 2ν.v)))
@@ -40,6 +35,35 @@ BulkModulus(G::ShearModulus, M::LongitudinalModulus) = BulkModulus(M.v - 4G.v / 
 BulkModulus(ν::PoissonRatio, M::LongitudinalModulus) = BulkModulus(M.v * (1 + ν.v) / (3 * (1 - ν.v)))
 BulkModulus(x::ElasticModulus, y::ElasticModulus) = BulkModulus(y, x)
 
-YoungModulus(E::YoungModulus, ::ElasticModulus) = E
+YoungModulus(K::BulkModulus, λ::Lame1stParameter) = YoungModulus(9K.v * (K.v - λ.v) / (3K.v - λ.v))
+YoungModulus(K::BulkModulus, G::ShearModulus) = YoungModulus(9K.v * G.v / (3K.v + G.v))
+YoungModulus(K::BulkModulus, ν::PoissonRatio) = YoungModulus(3K.v * (1 - 2ν.v))
+YoungModulus(K::BulkModulus, M::LongitudinalModulus) = YoungModulus(9K.v * (M.v - K.v) / (3K.v + M.v))
+YoungModulus(λ::Lame1stParameter, G::ShearModulus) = YoungModulus(G.v * (3λ.v + 2G.v) / (λ.v + G.v))
+YoungModulus(λ::Lame1stParameter, ν::PoissonRatio) = YoungModulus(λ.v * (1 + ν.v) * (1 - 2ν.v) / ν.v)
+YoungModulus(λ::Lame1stParameter, M::LongitudinalModulus) = YoungModulus((M.v - λ.v) * (M.v + 2λ.v) / (M.v + λ.v))
+YoungModulus(G::ShearModulus, ν::PoissonRatio) = YoungModulus(2G.v * (1 + ν.v))
+YoungModulus(G::ShearModulus, M::LongitudinalModulus) = YoungModulus(G.v * (3M.v - 4G.v) / (M.v - G.v))
+YoungModulus(ν::PoissonRatio, M::LongitudinalModulus) = YoungModulus(M.v * (1 + ν.v) * (1 - 2ν.v) / (1 - ν.v))
+
+# These are helper functions and should not be exported!
+_auxiliaryR(E::YoungModulus, λ::Lame1stParameter) = sqrt(E.v^2 + 9 * λ.v^2 + 2E.v * λ.v)
+_auxiliaryS(E::YoungModulus, M::LongitudinalModulus) = sqrt(E.v^2 + 9 * M.v^2 - 10E.v * M.v)
+
+for T in (
+    :BulkModulus,
+    :YoungModulus,
+    :Lame1stParameter,
+    :ShearModulus,
+    :PoissonRatio,
+    :LongitudinalModulus,
+)
+    eval(quote
+        # As long as creating from one parameter already defined, return the parameter itself.
+        $T(x::$T, ::ElasticModulus) = x
+        # If there is no method match, switch the parameters and try again.
+        $T(x::ElasticModulus, y::ElasticModulus) = $T(y, x)
+    end)
+end
 
 end # module Moduli
