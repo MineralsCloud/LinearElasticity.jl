d5c1c6d0c092bf8518f47f2065f1410cd38da76d
Reno <25192197+singularitti@users.noreply.github.com>
2021-03-24 12:39:10 -0400

    Change numbers to subscripts in src/StabilityConditions.jl
    & fix a bug in `Cubic` system

--- src/StabilityConditions.jl
+++ src/StabilityConditions.jl
@@ -10,91 +10,91 @@ using LinearElasticity: EngineeringStiffness, EngineeringCompliance
 export isstable
 
 function isstable(::Cubic, c::EngineeringStiffness)
-    c11, c12, c44 = c[1, 1], c[1, 2], c[1, 4]
+    c₁₁, c₁₂, c₄₄ = c[1, 1], c[1, 2], c[4, 4]
     return all((  # Must satisfy all criteria!
-        c11 > abs(c12),
-        c11 + 2 * c12 > 0,
-        c44 > 0,
+        c₁₁ > abs(c₁₂),
+        c₁₁ + 2 * c₁₂ > 0,
+        c₄₄ > 0,
     ))
 end
 function isstable(::Hexagonal, c::EngineeringStiffness)
-    c11, c12, c13, c33, c44, c66 = c[1, 1], c[1, 2], c[1, 3], c[3, 3], c[4, 4], c[6, 6]
+    c₁₁, c₁₂, c₁₃, c₃₃, c₄₄, c₆₆ = c[1, 1], c[1, 2], c[1, 3], c[3, 3], c[4, 4], c[6, 6]
     return all((  # Must satisfy all criteria!
-        c66 == (c11 - c12) / 2,
-        c11 > abs(c12),
-        2 * c13^2 < c33 * (c11 + c12),
-        c44 > 0,
-        c66 > 0,
+        c₆₆ == (c₁₁ - c₁₂) / 2,
+        c₁₁ > abs(c₁₂),
+        2 * c₁₃^2 < c₃₃ * (c₁₁ + c₁₂),
+        c₄₄ > 0,
+        c₆₆ > 0,
     ))
 end
 function isstable(::Tetragonal, c::EngineeringStiffness)
-    c11, c12, c13, c16, c33, c44, c66 =
+    c₁₁, c₁₂, c₁₃, c₁₆, c₃₃, c₄₄, c₆₆ =
         c[1, 1], c[1, 2], c[1, 3], c[1, 6], c[3, 3], c[4, 4], c[6, 6]
-    if c16 == 0  # Tetragonal (I) class
-        return all((c11 > abs(c12), 2 * c13^2 < c33 * (c11 + c12), c44 > 0, c66 > 0))
+    if c₁₆ == 0  # Tetragonal (I) class
+        return all((c₁₁ > abs(c₁₂), 2 * c₁₃^2 < c₃₃ * (c₁₁ + c₁₂), c₄₄ > 0, c₆₆ > 0))
     end
     return all((  # Tetragonal (II) class
-        c11 > abs(c12),
-        2 * c13^2 < c33 * (c11 + c12),
-        c44 > 0,
-        2 * c16^2 < c66 * (c11 - c12),
+        c₁₁ > abs(c₁₂),
+        2 * c₁₃^2 < c₃₃ * (c₁₁ + c₁₂),
+        c₄₄ > 0,
+        2 * c₁₆^2 < c₆₆ * (c₁₁ - c₁₂),
     ))
 end
 function isstable(::Trigonal, c::EngineeringStiffness)
-    c11, c12, c13, c14, c15, c33, c44, c66 =
+    c₁₁, c₁₂, c₁₃, c₁₄, c₁₅, c₃₃, c₄₄, c₆₆ =
         c[1, 1], c[1, 2], c[1, 3], c[1, 4], c[1, 5], c[3, 3], c[4, 4], c[6, 6]
-    if c15 == 0  # Rhombohedral (I) class
+    if c₁₅ == 0  # Rhombohedral (I) class
         return all((
-            c11 > abs(c12),
-            c44 > 0,
-            c13^2 < 0.5 * c33 * (c11 + c12),
-            c14^2 < 0.5 * c44 * (c11 - c12),
-            0.5 * c44 * (c11 - c12) == c44 * c66,
+            c₁₁ > abs(c₁₂),
+            c₄₄ > 0,
+            c₁₃^2 < 0.5 * c₃₃ * (c₁₁ + c₁₂),
+            c₁₄^2 < 0.5 * c₄₄ * (c₁₁ - c₁₂),
+            0.5 * c₄₄ * (c₁₁ - c₁₂) == c₄₄ * c₆₆,
         ))
     end
     return all((  # Rhombohedral (II) class
-        c11 > abs(c12),
-        c44 > 0,
-        c13^2 < 0.5 * c33 * (c11 + c12),
-        c14^2 + c15^2 < 0.5 * c44 * (c11 - c12),
-        0.5 * c44 * (c11 - c12) == c44 * c66,
+        c₁₁ > abs(c₁₂),
+        c₄₄ > 0,
+        c₁₃^2 < 0.5 * c₃₃ * (c₁₁ + c₁₂),
+        c₁₄^2 + c₁₅^2 < 0.5 * c₄₄ * (c₁₁ - c₁₂),
+        0.5 * c₄₄ * (c₁₁ - c₁₂) == c₄₄ * c₆₆,
     ))
 end
 function isstable(::Orthorhombic, c::EngineeringStiffness)
-    c11, c22, c33, c44, c55, c66 = diag(c)
-    c12, c13, c23 = c[1, 2], c[1, 3], c[2, 3]
+    c₁₁, c₂₂, c₃₃, c₄₄, c₅₅, c₆₆ = diag(c)
+    c₁₂, c₁₃, c₂₃ = c[1, 2], c[1, 3], c[2, 3]
     return all((
-        c11 > 0,
-        c11 * c22 > c12^2,
-        c11 * c22 * c33 + 2 * c12 * c13 * c23 > c11 * c23^2 + c22 * c13^2 + c33 * c12^2,
-        c44 > 0,
-        c55 > 0,
-        c66 > 0,
+        c₁₁ > 0,
+        c₁₁ * c₂₂ > c₁₂^2,
+        c₁₁ * c₂₂ * c₃₃ + 2 * c₁₂ * c₁₃ * c₂₃ > c₁₁ * c₂₃^2 + c₂₂ * c₁₃^2 + c₃₃ * c₁₂^2,
+        c₄₄ > 0,
+        c₅₅ > 0,
+        c₆₆ > 0,
     ))
 end
 function isstable(::Monoclinic, c::EngineeringStiffness)
-    c11, c22, c33, c44, c55, c66 = diag(c)
-    c12, c13, c15, c23, c25, c35, c46 =
+    c₁₁, c₂₂, c₃₃, c₄₄, c₅₅, c₆₆ = diag(c)
+    c₁₂, c₁₃, c₁₅, c₂₃, c₂₅, c₃₅, c₄₆ =
         c[1, 2], c[1, 3], c[1, 5], c[2, 3], c[2, 5], c[3, 5], c[4, 6]
     g =
-        c11 * c22 * c33 - c11 * c23 * c23 - c22 * c13 * c13 - c33 * c12 * c12 +
-        2 * c12 * c13 * c23
+        c₁₁ * c₂₂ * c₃₃ - c₁₁ * c₂₃ * c₂₃ - c₂₂ * c₁₃ * c₁₃ - c₃₃ * c₁₂ * c₁₂ +
+        2 * c₁₂ * c₁₃ * c₂₃
     return all((
-        all((c11, c22, c33, c44, c55, c66) .> 0),
-        c11 + c22 + c33 + 2 * (c12 + c13 + c23) > 0,
-        c33 * c55 - c35^2 > 0,
-        c44 * c66 - c46^2 > 0,
-        c22 + c33 - 2 * c23 > 0,
-        c22 * (c33 * c55 - c35^2) + 2 * c23 * c25 * c35 - c23^2 * c55 - c25^2 * c33 > 0,
+        all((c₁₁, c₂₂, c₃₃, c₄₄, c₅₅, c₆₆) .> 0),
+        c₁₁ + c₂₂ + c₃₃ + 2 * (c₁₂ + c₁₃ + c₂₃) > 0,
+        c₃₃ * c₅₅ - c₃₅^2 > 0,
+        c₄₄ * c₆₆ - c₄₆^2 > 0,
+        c₂₂ + c₃₃ - 2 * c₂₃ > 0,
+        c₂₂ * (c₃₃ * c₅₅ - c₃₅^2) + 2 * c₂₃ * c₂₅ * c₃₅ - c₂₃^2 * c₅₅ - c₂₅^2 * c₃₃ > 0,
         2 * (
-            c15 * c25 * (c33 * c12 - c13 * c23) +
-            c15 * c35 * (c22 * c13 - c12 * c23) +
-            c25 * c35 * (c11 * c23 - c12 * c13)
+            c₁₅ * c₂₅ * (c₃₃ * c₁₂ - c₁₃ * c₂₃) +
+            c₁₅ * c₃₅ * (c₂₂ * c₁₃ - c₁₂ * c₂₃) +
+            c₂₅ * c₃₅ * (c₁₁ * c₂₃ - c₁₂ * c₁₃)
         ) - (
-            c15 * c15 * (c22 * c33 - c23^2) +
-            c25 * c25 * (c11 * c33 - c13^2) +
-            c35 * c35 * (c11 * c22 - c12^2)
-        ) + c55 * g > 0,
+            c₁₅ * c₁₅ * (c₂₂ * c₃₃ - c₂₃^2) +
+            c₂₅ * c₂₅ * (c₁₁ * c₃₃ - c₁₃^2) +
+            c₃₅ * c₃₅ * (c₁₁ * c₂₂ - c₁₂^2)
+        ) + c₅₅ * g > 0,
     ))
 end
 isstable(C::CrystalSystem, s::EngineeringCompliance) = isstable(C, inv(s))
