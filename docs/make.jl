dd63113bc43cdcdf34595d65f6bae0f6aec51be7
Reno <25192197+singularitti@users.noreply.github.com>
2021-06-23 17:05:44 -0400

    Update docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,23 @@
 using LinearElasticity
 using Documenter
 
+DocMeta.setdocmeta!(LinearElasticity, :DocTestSetup, :(using LinearElasticity); recursive=true)
+
 makedocs(;
-    modules = [LinearElasticity],
-    authors = "Qi Zhang <singularitti@outlook.com>",
-    repo = "https://github.com/singularitti/LinearElasticity.jl/blob/{commit}{path}#L{line}",
-    sitename = "LinearElasticity.jl",
-    format = Documenter.HTML(;
-        prettyurls = get(ENV, "CI", "false") == "true",
-        canonical = "https://singularitti.github.io/LinearElasticity.jl",
-        assets = String[],
+    modules=[LinearElasticity],
+    authors="Qi Zhang <singularitti@outlook.com>",
+    repo="https://github.com/MineralsCloud/LinearElasticity.jl/blob/{commit}{path}#{line}",
+    sitename="LinearElasticity.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://MineralsCloud.github.io/LinearElasticity.jl",
+        assets=String[],
     ),
-    pages = ["Home" => "index.md"],
+    pages=[
+        "Home" => "index.md",
+    ],
 )
 
-deploydocs(; repo = "github.com/singularitti/LinearElasticity.jl")
+deploydocs(;
+    repo="github.com/MineralsCloud/LinearElasticity.jl",
+)
