efb11b9fce2e110ab56f277c80d90283508e2746
Reno <25192197+singularitti@users.noreply.github.com>
2021-10-26 16:49:47 -0400

    Move things to src/symmetry_criteria.jl

--- src/LinearElasticity.jl
+++ src/LinearElasticity.jl
@@ -45,98 +45,6 @@ struct EngineeringCompliance{T} <: Compliance{T,2}
     data::SHermitianCompact{6,T}
 end
 
-function symmetry_criteria(::Cubic, c::EngineeringStiffness)
-    return all((
-        issymmetric(c),
-        c[1, 1] == c[2, 2] == c[3, 3],
-        c[4, 4] == c[5, 5] == c[6, 6],
-        c[1, 2] == c[1, 3] == c[2, 3],
-    ))
-end
-function symmetry_criteria(::Hexagonal, c::EngineeringStiffness)
-    return all((
-        issymmetric(c),
-        c[1, 1] == c[2, 2],
-        c[4, 4] == c[5, 5],
-        c[1, 3] == c[2, 3],
-        2c[6, 6] == c[1, 1] - c[1, 2],
-    ))
-end
-function symmetry_criteria(::Tetragonal, c::EngineeringStiffness)
-    return all((
-        issymmetric(c),
-        c[1, 1] == c[2, 2],
-        c[4, 4] == c[5, 5],
-        c[1, 3] == c[2, 3],
-        if iszero(c[1, 6])  # Tetragonal (I) class, 4mm, -42m, 422, 4/mmm
-            true
-        else  # Tetragonal (II) class, 4, -4, 4/m
-            c[1, 6] == -c[2, 6]
-        end,
-    ))
-end
-function symmetry_criteria(::Trigonal, c::EngineeringStiffness)
-    return all((
-        issymmetric(c),
-        c[1, 1] == c[2, 2],
-        c[4, 4] == c[5, 5],
-        c[1, 3] == c[2, 3],
-        2c[6, 6] == c[1, 1] - c[1, 2],
-        c[1, 4] == -c[2, 4] == -c[5, 6],
-        if iszero(c[1, 5])  # # Rhombohedral (I) class, 32, -3m, 3m
-            true
-        else  # Rhombohedral (II) class, 3, -3
-            -c[1, 5] == c[2, 5] == c[4, 6]
-        end,
-    ))
-end
-function symmetry_criteria(::Orthorhombic, c::EngineeringStiffness)
-    return all((
-        issymmetric(c),
-        all(
-            iszero,
-            (
-                c[1, 4],
-                c[2, 4],
-                c[3, 4],
-                c[1, 5],
-                c[2, 5],
-                c[3, 5],
-                c[4, 5],
-                c[1, 6],
-                c[2, 6],
-                c[3, 6],
-                c[4, 6],
-                c[5, 6],
-            ),
-        ),
-        all(
-            !iszero,
-            (
-                c[1, 1],
-                c[1, 2],
-                c[1, 3],
-                c[2, 2],
-                c[2, 3],
-                c[3, 3],
-                c[4, 4],
-                c[5, 5],
-                c[6, 6],
-            ),
-        ),
-    ))
-end
-function symmetry_criteria(::Monoclinic, c::EngineeringStiffness)
-    return all((
-        issymmetric(c),
-        all(
-            iszero,
-            (c[1, 4], c[2, 4], c[3, 4], c[4, 5], c[1, 6], c[2, 6], c[3, 6], c[5, 6]),
-        ),
-    ))
-end
-symmetry_criteria(C::CrystalSystem, s::EngineeringCompliance) = symmetry_criteria(C, inv(s))
-
 issystem(C::CrystalSystem, x::Union{EngineeringStiffness,EngineeringCompliance}) =
     all(symmetry_criteria(C, x))
 
@@ -208,6 +116,7 @@ const VOIGT_INDICES =
     ((1, 1), (2, 2), (3, 3), (3, 2), (3, 1), (2, 1), (2, 3), (1, 3), (1, 2))
 
 include("conversion.jl")
+include("symmetry_criteria.jl")
 include("StabilityCriteria.jl")
 include("Moduli.jl")
 
