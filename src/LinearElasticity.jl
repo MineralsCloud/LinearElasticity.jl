424924e4f7bc25f0f8bad3a9a8df720e934ae6d4
Reno <25192197+singularitti@users.noreply.github.com>
2021-10-26 16:51:54 -0400

    Move `issystem`, `principal_values`, `principal_invariants`, `main_invariants`, etc., to src/utils.jl

--- src/LinearElasticity.jl
+++ src/LinearElasticity.jl
@@ -45,28 +45,6 @@ struct EngineeringCompliance{T} <: Compliance{T,2}
     data::SHermitianCompact{6,T}
 end
 
-issystem(C::CrystalSystem, x::Union{EngineeringStiffness,EngineeringCompliance}) =
-    all(symmetry_criteria(C, x))
-
-principal_values(x::Union{Stress,Strain}) = eigvals(x)
-principal_axes(x::Union{Stress,Strain}) = eigvecs(x)
-principal_invariants(x::Union{Stress,Strain}, n::Int) = principal_invariants(x, Val(n))
-principal_invariants(x::Union{Stress,Strain}, ::Val{1}) = tr(x)
-function principal_invariants(x::Union{Stress,Strain}, ::Val{2})
-    λ1, λ2, λ3 = principal_values(x)
-    return λ1 * λ2 + λ1 * λ3 + λ2 * λ3
-end # function principal_invariants
-principal_invariants(x::Union{Stress,Strain}, ::Val{3}) = det(x)
-
-main_invariants(x::Union{Stress,Strain}, n::Int) = main_invariants(x, Val(n))
-main_invariants(x::Union{Stress,Strain}, ::Val{1}) = principal_invariants(x, 1)
-main_invariants(x::Union{Stress,Strain}, ::Val{2}) =
-    principal_invariants(x, 1)^2 - 2 * principal_invariants(x, 2)
-main_invariants(x::Union{Stress,Strain}, ::Val{3}) =
-    principal_invariants(x, 1)^3 +
-    3 *
-    (principal_invariants(x, 3) - principal_invariants(x, 1) * principal_invariants(x, 2))
-
 Base.size(::Union{TensorStress,TensorStrain}) = (3, 3)
 Base.size(::Union{TensorStiffness,TensorCompliance}) = (3, 3, 3, 3)
 Base.size(::Union{EngineeringStress,EngineeringStrain}) = (6,)
@@ -117,6 +95,7 @@ const VOIGT_INDICES =
 
 include("conversion.jl")
 include("symmetry_criteria.jl")
+include("utils.jl")
 include("StabilityCriteria.jl")
 include("Moduli.jl")
 
---
+++ src/utils.jl
@@ -0,0 +1,21 @@
+issystem(C::CrystalSystem, x::Union{EngineeringStiffness,EngineeringCompliance}) =
+    all(symmetry_criteria(C, x))
+
+principal_values(x::Union{Stress,Strain}) = eigvals(x)
+principal_axes(x::Union{Stress,Strain}) = eigvecs(x)
+principal_invariants(x::Union{Stress,Strain}, n::Int) = principal_invariants(x, Val(n))
+principal_invariants(x::Union{Stress,Strain}, ::Val{1}) = tr(x)
+function principal_invariants(x::Union{Stress,Strain}, ::Val{2})
+    λ1, λ2, λ3 = principal_values(x)
+    return λ1 * λ2 + λ1 * λ3 + λ2 * λ3
+end # function principal_invariants
+principal_invariants(x::Union{Stress,Strain}, ::Val{3}) = det(x)
+
+main_invariants(x::Union{Stress,Strain}, n::Int) = main_invariants(x, Val(n))
+main_invariants(x::Union{Stress,Strain}, ::Val{1}) = principal_invariants(x, 1)
+main_invariants(x::Union{Stress,Strain}, ::Val{2}) =
+    principal_invariants(x, 1)^2 - 2 * principal_invariants(x, 2)
+main_invariants(x::Union{Stress,Strain}, ::Val{3}) =
+    principal_invariants(x, 1)^3 +
+    3 *
+    (principal_invariants(x, 3) - principal_invariants(x, 1) * principal_invariants(x, 2))
