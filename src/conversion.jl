9b2adef3ba9bb009f0995fef7154bcfe798061af
Reno <25192197+singularitti@users.noreply.github.com>
2021-10-30 03:02:57 -0400

    Fix `convert` for `StiffnessMatrix` & `ComplianceMatrix`

--- src/conversion.jl
+++ src/conversion.jl
@@ -14,11 +14,54 @@ Base.convert(::Type{TensorStrain{T}}, ϵ::EngineeringStrain{T}) where {T} =
     TensorStrain([ϵ[1], ϵ[6] / 2, ϵ[5] / 2, ϵ[2], ϵ[4] / 2, ϵ[3]])
 Base.convert(::Type{EngineeringStrain{T}}, ε::TensorStrain{T}) where {T} =
     EngineeringStrain([ε[1, 1], ε[2, 2], ε[3, 3], 2ε[2, 3], 2ε[1, 3], 2ε[1, 2]])
-function Base.convert(::Type{StiffnessMatrix{T}}, c::StiffnessTensor{T}) where {T}
-    p = pairs(VOIGT_INDICES)
-    # From https://github.com/KristofferC/Tensors.jl/blob/bff451c/src/utilities.jl#L5-L14
-    return StiffnessMatrix([c[p[i]..., p[j]...] for i in 1:6, j in 1:6])
-end
+Base.convert(::Type{StiffnessMatrix{T}}, c::StiffnessTensor{T}) where {T} =
+    StiffnessMatrix([
+        c[1, 1, 1, 1],
+        c[1, 1, 2, 2],
+        c[1, 1, 3, 3],
+        c[1, 1, 2, 3],
+        c[1, 1, 1, 3],
+        c[1, 1, 1, 2],
+        c[2, 2, 2, 2],
+        c[2, 2, 3, 3],
+        c[2, 2, 2, 3],
+        c[2, 2, 1, 3],
+        c[2, 2, 1, 2],
+        c[3, 3, 3, 3],
+        c[3, 3, 2, 3],
+        c[3, 3, 1, 3],
+        c[3, 3, 1, 2],
+        c[2, 3, 2, 3],
+        c[2, 3, 1, 3],
+        c[2, 3, 1, 2],
+        c[1, 3, 1, 3],
+        c[1, 3, 1, 2],
+        c[1, 2, 1, 2],
+    ])
+Base.convert(::Type{ComplianceMatrix{T}}, s::ComplianceTensor{T}) where {T} =
+    ComplianceMatrix([
+        s[1, 1, 1, 1],
+        s[1, 1, 2, 2],
+        s[1, 1, 3, 3],
+        s[1, 1, 2, 3] / 2,
+        s[1, 1, 1, 3] / 2,
+        s[1, 1, 1, 2] / 2,
+        s[2, 2, 2, 2],
+        s[2, 2, 3, 3],
+        s[2, 2, 2, 3] / 2,
+        s[2, 2, 1, 3] / 2,
+        s[2, 2, 1, 2] / 2,
+        s[3, 3, 3, 3],
+        s[3, 3, 2, 3] / 2,
+        s[3, 3, 1, 3] / 2,
+        s[3, 3, 1, 2] / 2,
+        s[2, 3, 2, 3] / 4,
+        s[2, 3, 1, 3] / 4,
+        s[2, 3, 1, 2] / 4,
+        s[1, 3, 1, 3] / 4,
+        s[1, 3, 1, 2] / 4,
+        s[1, 2, 1, 2] / 4,
+    ])
 function Base.convert(::Type{StiffnessTensor{T}}, c::StiffnessMatrix{T}) where {T}
     d = Dict(zip(VOIGT_INDICES, [1, 2, 3, 4, 5, 6, 4, 5, 6]))
     return StiffnessTensor([
