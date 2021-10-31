110237336b8fb71b3ab9f5054096409aeeb2613c
Reno <25192197+singularitti@users.noreply.github.com>
2021-10-31 06:34:38 -0400

    Move `issystem` to src/symmetry_criteria.jl

--- src/invariants.jl
+++ src/invariants.jl
@@ -2,10 +2,7 @@ using LinearAlgebra: eigvals, eigvecs
 using Tensorial: stress_invariants, deviatoric_stress_invariants, vol, dev
 
 export principal_values,
-    principal_axes, principal_invariants, main_invariants, hydrostatic, deviatoric, issystem
-
-issystem(C::CrystalSystem, x::Union{StiffnessMatrix,ComplianceMatrix}) =
-    all(symmetry_criteria(C, x))
+    principal_axes, principal_invariants, main_invariants, hydrostatic, deviatoric
 
 principal_values(x::Union{Stress,Strain}) = eigvals(x)
 
