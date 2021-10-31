6a9f944de91f203ed9311eeb796f9585c52d7b44
Reno <25192197+singularitti@users.noreply.github.com>
2021-10-31 03:04:26 -0400

    Fix `principal_invariants`

--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,5 @@
-using LinearAlgebra: tr, det, eigvals, eigvecs
+using LinearAlgebra: eigvals, eigvecs
+using Tensorial: stress_invariants, deviatoric_stress_invariants
 
 export principal_values, principal_axes, principal_invariants, main_invariants, issystem
 
@@ -9,13 +10,7 @@ principal_values(x::Union{Stress,Strain}) = eigvals(x)
 
 principal_axes(x::Union{Stress,Strain}) = eigvecs(x)
 
-principal_invariants(x::Union{Stress,Strain}, n::Int) = principal_invariants(x, Val(n))
-principal_invariants(x::Union{Stress,Strain}, ::Val{1}) = tr(x)
-function principal_invariants(x::Union{Stress,Strain}, ::Val{2})
-    λ1, λ2, λ3 = principal_values(x)
-    return λ1 * λ2 + λ1 * λ3 + λ2 * λ3
-end # function principal_invariants
-principal_invariants(x::Union{Stress,Strain}, ::Val{3}) = det(x)
+principal_invariants(x::Union{TensorStress,TensorStrain}) = stress_invariants(x.data)
 
 main_invariants(x::Union{Stress,Strain}, n::Int) = main_invariants(x, Val(n))
 main_invariants(x::Union{Stress,Strain}, ::Val{1}) = principal_invariants(x, 1)
