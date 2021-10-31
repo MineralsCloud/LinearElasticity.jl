e62eea4ca12017a8d5001f47a764b6197ae5a005
Reno <25192197+singularitti@users.noreply.github.com>
2021-10-31 05:52:24 -0400

    Add `hydrostatic` & `deviatoric`

--- src/invariants.jl
+++ src/invariants.jl
@@ -1,7 +1,8 @@
 using LinearAlgebra: eigvals, eigvecs
-using Tensorial: stress_invariants, deviatoric_stress_invariants
+using Tensorial: stress_invariants, deviatoric_stress_invariants, vol, dev
 
-export principal_values, principal_axes, principal_invariants, main_invariants, issystem
+export principal_values,
+    principal_axes, principal_invariants, main_invariants, hydrostatic, deviatoric, issystem
 
 issystem(C::CrystalSystem, x::Union{StiffnessMatrix,ComplianceMatrix}) =
     all(symmetry_criteria(C, x))
@@ -13,3 +14,7 @@ principal_axes(x::Union{Stress,Strain}) = eigvecs(x)
 principal_invariants(x::Union{TensorStress,TensorStrain}) = stress_invariants(x.data)
 
 main_invariants(x::Union{Stress,Strain}) = deviatoric_stress_invariants(x.data)
+
+hydrostatic(x::Union{TensorStress,TensorStrain}) = vol(x.data)
+
+deviatoric(x::Union{TensorStress,TensorStrain}) = dev(x.data)
