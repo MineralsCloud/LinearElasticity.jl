30142e6e616743c3902950b440cbad3c09afb04d
Reno <25192197+singularitti@users.noreply.github.com>
2021-11-01 04:15:25 -0400

    Include "Tests from homework 2"

--- test/invariants.jl
+++ test/invariants.jl
@@ -1,4 +1,5 @@
 using LinearAlgebra: diagm, norm
+# using Unitful: @u_str
 
 # Example from https://www.continuummechanics.org/hydrodeviatoricstrain.html
 @testset "Test `hydrostatic` and `deviatoric`" begin
@@ -41,4 +42,32 @@ end
     @test principal_invariants(E)[1] == 0.4
     @test principal_invariants(E)[:I2] ≈ -0.21
     @test principal_invariants(E)[:I3] == -0.028
+end
+
+# @testset "Tests from homework 1" begin
+#     σ = TensorStress([
+#         300 100 0
+#         100 100 0
+#         0 0 0
+#     ] * u"MPa")
+#     @test principal_values(E)[1] == (200 + 100 √ 2) * u"MPa"
+#     @test principal_values(E)[2] == (200 - 100 √ 2) * u"MPa"
+# end
+
+@testset "Tests from homework 2" begin
+    σ₂₂ = 200
+    σ₁₂ = σ₂₃ = 141
+    σ = TensorStress([
+        0 σ₁₂ 0
+        σ₁₂ σ₂₂ σ₂₃
+        0 σ₂₃ 0
+    ])
+    @test principal_values(σ)[1] == -123.07397876041034
+    @test isapprox(principal_values(σ)[2], 0; atol = 1e-13)
+    @test principal_values(σ)[3] == 323.07397876041034
+    @test norm(principal_axes(σ) - [
+        0.601723   0.707107     0.371389
+        -0.525223   0  0.850965
+         0.601723  -0.707107     0.371389
+    ]) < 1e-6
 end
