f5f44c9dccaf39863aaa8660526e66b29d3624a7
Reno <25192197+singularitti@users.noreply.github.com>
2021-11-01 02:54:33 -0400

    Add "Test unitary transformation"

--- test/invariants.jl
+++ test/invariants.jl
@@ -1,5 +1,6 @@
-using LinearAlgebra: diagm
+using LinearAlgebra: diagm, norm
 
+# Example from https://www.continuummechanics.org/hydrodeviatoricstrain.html
 @testset "Test `hydrostatic` and `deviatoric`" begin
     E = TensorStrain([
         0.5 0.3 0.2
@@ -9,4 +10,21 @@ using LinearAlgebra: diagm
     @test hydrostatic(E) == TensorStrain(diagm([2, 2, 2] / 15))
     @test deviatoric(E) == TensorStrain(E - diagm([2, 2, 2] / 15))
     @test TensorStrain(hydrostatic(E) + deviatoric(E)) ≈ E
+end
+
+@testset "Test unitary transformation" begin
+    E = TensorStrain([
+        0.5 0.3 0.2
+        0.3 -0.2 -0.1
+        0.2 -0.1 0.1
+    ])
+    evecs = principal_axes(E)
+    normal_strains = transpose(evecs) * E * evecs
+    @test norm(normal_strains - [
+        -0.370577 0 0
+        0 0.115308 0
+        0 0 0.655269
+    ]) < 1e-6
+    @test isapprox(principal_values(E), [-0.370577, 0.115308, 0.655269]; atol = 1e-6)
+    @test tr(normal_strains) ≈ tr(E) ≈ 2 / 5
 end
