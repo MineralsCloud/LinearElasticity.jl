11abb831c7718f59a0b7f6e810c236f8f0146659
Reno <25192197+singularitti@users.noreply.github.com>
2021-11-04 17:20:27 -0400

    Fix type constraints on `main_invariants`, `principal_axes` & `principal_values`

--- src/invariants.jl
+++ src/invariants.jl
@@ -4,13 +4,13 @@ using Tensorial: stress_invariants, deviatoric_stress_invariants, vol, dev
 export principal_values,
     principal_axes, principal_invariants, main_invariants, hydrostatic, deviatoric
 
-principal_values(x::Union{Stress,Strain}) = eigvals(x)
+principal_values(x::Union{TensorStress,TensorStrain}) = eigvals(x)
 
-principal_axes(x::Union{Stress,Strain}) = eigvecs(x)
+principal_axes(x::Union{TensorStress,TensorStrain}) = eigvecs(x)
 
 principal_invariants(x::Union{TensorStress,TensorStrain}) = stress_invariants(x.data)
 
-main_invariants(x::Union{Stress,Strain}) = deviatoric_stress_invariants(x.data)
+main_invariants(x::Union{TensorStress,TensorStrain}) = deviatoric_stress_invariants(x.data)
 
 hydrostatic(x::Union{TensorStress,TensorStrain}) = vol(x.data)
 
