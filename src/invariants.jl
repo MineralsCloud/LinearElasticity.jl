453c2aa252f305742c424514dcdf4618c37541c7
Reno <25192197+singularitti@users.noreply.github.com>
2021-11-05 13:24:41 -0400

    Fix `principal_values` & `principal_axes`
    Need https://github.com/PainterQubits/Unitful.jl/issues/46 to be solved

--- src/invariants.jl
+++ src/invariants.jl
@@ -1,12 +1,12 @@
-using LinearAlgebra: eigvals, eigvecs
+using LinearAlgebra: Eigen, eigen
 using Tensorial: stress_invariants, deviatoric_stress_invariants, vol, dev
 
 export principal_values,
     principal_axes, principal_invariants, main_invariants, hydrostatic, deviatoric
 
-principal_values(x::Union{TensorStress,TensorStrain}) = eigvals(x)
+principal_values(x::Union{TensorStress,TensorStrain}) = _eigen(x).values
 
-principal_axes(x::Union{TensorStress,TensorStrain}) = eigvecs(x)
+principal_axes(x::Union{TensorStress,TensorStrain}) = _eigen(x).vectors
 
 principal_invariants(x::Union{TensorStress,TensorStrain}) = stress_invariants(x.data)
 
@@ -17,4 +17,10 @@ for T in (:TensorStress, :TensorStrain)
         hydrostatic(x::$T) = $T(vol(SymmetricSecondOrderTensor{3}(float(x))))
         deviatoric(x::$T) = $T(dev(SymmetricSecondOrderTensor{3}(float(x))))
     end
+end
+
+function _eigen(x)
+    unitless = @. x / oneunit(x)
+    eg = eigen(unitless)
+    return Eigen(eg.values * oneunit(x[1]), eg.vectors)
 end
