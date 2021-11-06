482c153daf29bc23dcd5e8f2f326faeefca66b18
Reno <25192197+singularitti@users.noreply.github.com>
2021-11-06 17:58:04 -0400

    Remove `all` in `symmetry_criteria`

--- src/symmetry_criteria.jl
+++ src/symmetry_criteria.jl
@@ -4,22 +4,22 @@ using CrystallographyBase:
 export issystem
 
 function symmetry_criteria(::Cubic, c::StiffnessMatrix)
-    return all((
+    return (
         c[1, 1] == c[2, 2] == c[3, 3],
         c[4, 4] == c[5, 5] == c[6, 6],
         c[1, 2] == c[1, 3] == c[2, 3],
-    ))
+    )
 end
 function symmetry_criteria(::Hexagonal, c::StiffnessMatrix)
-    return all((
+    return (
         c[1, 1] == c[2, 2],
         c[4, 4] == c[5, 5],
         c[1, 3] == c[2, 3],
         2c[6, 6] == c[1, 1] - c[1, 2],
-    ))
+    )
 end
 function symmetry_criteria(::Tetragonal, c::StiffnessMatrix)
-    return all((
+    return (
         c[1, 1] == c[2, 2],
         c[4, 4] == c[5, 5],
         c[1, 3] == c[2, 3],
@@ -28,10 +28,10 @@ function symmetry_criteria(::Tetragonal, c::StiffnessMatrix)
         else  # Tetragonal (II) class, 4, -4, 4/m
             c[1, 6] == -c[2, 6]
         end,
-    ))
+    )
 end
 function symmetry_criteria(::Trigonal, c::StiffnessMatrix)
-    return all((
+    return (
         c[1, 1] == c[2, 2],
         c[4, 4] == c[5, 5],
         c[1, 3] == c[2, 3],
@@ -42,10 +42,10 @@ function symmetry_criteria(::Trigonal, c::StiffnessMatrix)
         else  # Rhombohedral (II) class, 3, -3
             -c[1, 5] == c[2, 5] == c[4, 6]
         end,
-    ))
+    )
 end
 function symmetry_criteria(::Orthorhombic, c::StiffnessMatrix)
-    return all((
+    return (
         all(
             iszero,
             (
@@ -77,15 +77,15 @@ function symmetry_criteria(::Orthorhombic, c::StiffnessMatrix)
                 c[6, 6],
             ),
         ),
-    ))
+    )
 end
 function symmetry_criteria(::Monoclinic, c::StiffnessMatrix)
-    return all((
+    return (
         all(
             iszero,
             (c[1, 4], c[2, 4], c[3, 4], c[4, 5], c[1, 6], c[2, 6], c[3, 6], c[5, 6]),
         ),
-    ))
+    )
 end
 symmetry_criteria(C::CrystalSystem, s::ComplianceMatrix) = symmetry_criteria(C, inv(s))
 
