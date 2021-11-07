dd78bf3192dcd3c042b54a7477e3debd0883b9a6
Reno <25192197+singularitti@users.noreply.github.com>
2021-11-07 01:24:21 -0400

    Fix `symmetry_criteria` for `ComplianceMatrix`

--- src/SymmetryCriteria.jl
+++ src/SymmetryCriteria.jl
@@ -13,13 +13,13 @@ using ..LinearElasticity: StiffnessMatrix, ComplianceMatrix
 
 export issystem, whichsystem
 
-function symmetry_criteria(::Cubic, c::StiffnessMatrix)
+function symmetry_criteria(::Cubic, x::Union{StiffnessMatrix,ComplianceMatrix})
     return (
-        all(iszero, (c[1:3, 4:6]..., c[4, 5:6]..., c[5, 6])),
-        all(!iszero, (c[1, 1], c[4, 4], c[1, 3])),
-        c[1, 1] == c[2, 2] == c[3, 3],
-        c[4, 4] == c[5, 5] == c[6, 6],
-        c[1, 2] == c[1, 3] == c[2, 3],
+        all(iszero, (x[1:3, 4:6]..., x[4, 5:6]..., x[5, 6])),
+        all(!iszero, (x[1, 1], x[4, 4], x[1, 3])),
+        x[1, 1] == x[2, 2] == x[3, 3],
+        x[4, 4] == x[5, 5] == x[6, 6],
+        x[1, 2] == x[1, 3] == x[2, 3],
     )
 end
 function symmetry_criteria(::Hexagonal, c::StiffnessMatrix)
@@ -32,17 +32,27 @@ function symmetry_criteria(::Hexagonal, c::StiffnessMatrix)
         2c[6, 6] == c[1, 1] - c[1, 2],
     )
 end
-function symmetry_criteria(::Tetragonal, c::StiffnessMatrix)
+function symmetry_criteria(::Hexagonal, s::ComplianceMatrix)
     return (
-        all(iszero, (c[1:3, 4:5]..., c[3, 6], c[4, 5:6]..., c[5, 6])),
-        all(!iszero, (c[1, 1], c[1, 2], c[1, 3], c[3, 3], c[4, 4], c[6, 6])),
-        c[1, 1] == c[2, 2],
-        c[4, 4] == c[5, 5],
-        c[1, 3] == c[2, 3],
-        if all(iszero, (c[2, 6], c[1, 6]))  # Tetragonal (I) class, 4mm, -42m, 422, 4/mmm
+        all(iszero, (s[1:3, 4:6]..., s[4, 5:6]..., s[5, 6])),
+        all(!iszero, (s[1, 1], s[3, 3], s[4, 4], s[1, 2], s[1, 3], s[6, 6])),
+        s[1, 1] == s[2, 2],
+        s[4, 4] == s[5, 5],
+        s[1, 3] == s[2, 3],
+        s[6, 6] == 2(s[1, 1] - s[1, 2]),
+    )
+end
+function symmetry_criteria(::Tetragonal, x::Union{StiffnessMatrix,ComplianceMatrix})
+    return (
+        all(iszero, (x[1:3, 4:5]..., x[3, 6], x[4, 5:6]..., x[5, 6])),
+        all(!iszero, (x[1, 1], x[1, 2], x[1, 3], x[3, 3], x[4, 4], x[6, 6])),
+        x[1, 1] == x[2, 2],
+        x[4, 4] == x[5, 5],
+        x[1, 3] == x[2, 3],
+        if all(iszero, (x[2, 6], x[1, 6]))  # Tetragonal (I) class, 4mm, -42m, 422, 4/mmm
             true
         else  # Tetragonal (II) class, 4, -4, 4/m
-            c[1, 6] == -c[2, 6]
+            x[1, 6] == -x[2, 6]
         end,
     )
 end
@@ -60,24 +70,23 @@ function symmetry_criteria(::Trigonal, c::StiffnessMatrix)
         end,
     )
 end
-function symmetry_criteria(::Orthorhombic, c::StiffnessMatrix)
+function symmetry_criteria(::Orthorhombic, x::Union{StiffnessMatrix,ComplianceMatrix})
     return (
-        all(iszero, (c[1:3, 4:6]..., c[4, 5:6]..., c[5, 6])),
-        all(!iszero, (c[1, 1:3]..., c[2, 2:3]..., c[3, 3], c[4, 4], c[5, 5], c[6, 6])),
+        all(iszero, (x[1:3, 4:6]..., x[4, 5:6]..., x[5, 6])),
+        all(!iszero, (x[1, 1:3]..., x[2, 2:3]..., x[3, 3], x[4, 4], x[5, 5], x[6, 6])),
     )
 end
-function symmetry_criteria(::Monoclinic, c::StiffnessMatrix)
+function symmetry_criteria(::Monoclinic, x::Union{StiffnessMatrix,ComplianceMatrix})
     return (
-        all(iszero, (c[1:3, 4]..., c[5, 6])),
-        all(!iszero, (c[1, 1:3]..., c[2, 2:3]..., c[3, 3], c[4, 4], c[5, 5], c[6, 6])),
-        if iszero(c[4, 5])  # Diad // x2, standard orientation
-            all(iszero, c[1:3, 6]) && all(!iszero, c[1:3, 5]) && !iszero(c[4, 6])
+        all(iszero, (x[1:3, 4]..., x[5, 6])),
+        all(!iszero, (x[1, 1:3]..., x[2, 2:3]..., x[3, 3], x[4, 4], x[5, 5], x[6, 6])),
+        if iszero(x[4, 5])  # Diad // x2, standard orientation
+            all(iszero, x[1:3, 6]) && all(!iszero, x[1:3, 5]) && !iszero(x[4, 6])
         else
-            all(!iszero, c[1:3, 6]) && all(iszero, c[1:3, 5]) && iszero(c[4, 6])
+            all(!iszero, x[1:3, 6]) && all(iszero, x[1:3, 5]) && iszero(x[4, 6])
         end,
     )
 end
-symmetry_criteria(C::CrystalSystem, s::ComplianceMatrix) = symmetry_criteria(C, inv(s))
 
 issystem(x::Union{StiffnessMatrix,ComplianceMatrix}, system::CrystalSystem) =
     all(symmetry_criteria(system, x))
