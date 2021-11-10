e73e7b8f86f2a7b8fa51696e13754fda89ccf5c5
Reno <25192197+singularitti@users.noreply.github.com>
2021-11-10 12:58:43 -0500

    Update links to src/distort.jl

--- src/distort.jl
+++ src/distort.jl
@@ -3,7 +3,7 @@ using LinearAlgebra: I
 
 export distort
 
-# See https://link.springer.com/content/pdf/10.1007%2F978-3-7091-0382-1_7.pdf
+# See https://link.springer.com/content/pdf/10.1007%2F978-3-7091-0382-1_7.pdf and https://doi.org/10.2138/am-1997-1-207
 distort(lattice::Lattice, strain::TensorStrain) = Lattice((I + strain.data) * lattice.data)
 distort(lattice::Lattice, strain::EngineeringStrain) =
     distort(lattice, TensorStrain(strain))
