4955409e4860bc3d5b2a2249179c2a86dbc47d9b
Reno <25192197+singularitti@users.noreply.github.com>
2021-11-14 01:21:49 -0500

    Add 2 more tests in "Test `*` and `/`"

--- test/arithmetic.jl
+++ test/arithmetic.jl
@@ -8,6 +8,8 @@ end
 
 @testset "Test `*` and `/`" begin
     a = EngineeringStrain(1:6)
+    @test typeof(2a) == EngineeringStrain{Int}
+    @test typeof(2.0a) == EngineeringStrain{Float64}
     @test 2a == 2 * a == a * 2 == 2.0a == EngineeringStress(2:2:12)
     @test a / 2 == EngineeringStress(0.5, 1, 1.5, 2, 2.5, 3)
     @test 2a / 2 == a
