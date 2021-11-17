9be8ef61c7d90e29497974c30372fbbcb92baa85
Reno <25192197+singularitti@users.noreply.github.com>
2021-11-17 13:24:40 -0500

    Rename `distort` to `distortby`
    add alias `distort` for the sake of compatibility

--- src/distort.jl
+++ src/distort.jl
@@ -1,12 +1,14 @@
 using CrystallographyBase: Lattice, CrystalSystem, Cubic, Hexagonal
 using LinearAlgebra: I, norm, dot
 
-export ElasticConstantFitter, distort
+export ElasticConstantFitter, distortby, distort
 
 # See https://link.springer.com/content/pdf/10.1007%2F978-3-7091-0382-1_7.pdf and https://doi.org/10.2138/am-1997-1-207
-distort(lattice::Lattice, strain::TensorStrain) = Lattice((I + strain.data) * lattice.data)
-distort(lattice::Lattice, strain::EngineeringStrain) =
-    distort(lattice, TensorStrain(strain))
+distortby(lattice::Lattice, strain::TensorStrain) =
+    Lattice((I + strain.data) * lattice.data)
+distortby(lattice::Lattice, strain::EngineeringStrain) =
+    distortby(lattice, TensorStrain(strain))
+const distort = distortby  # For the sake of compatibility
 
 struct ElasticConstantFitter{T<:CrystalSystem}
     system::T
