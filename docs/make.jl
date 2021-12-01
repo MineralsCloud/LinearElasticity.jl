835b8bc4510a5e1aeb072d1807972af5e6870cf7
singularitti <singularitti@users.noreply.github.com>
2021-12-01 00:15:17 +0000

    Format .jl files

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,24 @@
 using LinearElasticity
 using Documenter
 
-DocMeta.setdocmeta!(LinearElasticity, :DocTestSetup, :(using LinearElasticity); recursive=true)
+DocMeta.setdocmeta!(
+    LinearElasticity,
+    :DocTestSetup,
+    :(using LinearElasticity);
+    recursive = true,
+)
 
 makedocs(;
-    modules=[LinearElasticity],
-    authors="Qi Zhang <singularitti@outlook.com>",
-    repo="https://github.com/MineralsCloud/LinearElasticity.jl/blob/{commit}{path}#{line}",
-    sitename="LinearElasticity.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://MineralsCloud.github.io/LinearElasticity.jl",
-        assets=String[],
+    modules = [LinearElasticity],
+    authors = "Qi Zhang <singularitti@outlook.com>",
+    repo = "https://github.com/MineralsCloud/LinearElasticity.jl/blob/{commit}{path}#{line}",
+    sitename = "LinearElasticity.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://MineralsCloud.github.io/LinearElasticity.jl",
+        assets = String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/MineralsCloud/LinearElasticity.jl",
-)
+deploydocs(; repo = "github.com/MineralsCloud/LinearElasticity.jl")
