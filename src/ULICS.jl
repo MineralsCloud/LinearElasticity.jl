d3dc5d2b6bb9867b31023ffd72f86ff9e47c3c30
Reno <singularitti@outlook.com>
2022-07-09 02:44:17 -0400

    Add module `ULICS`

---
+++ src/ULICS.jl
@@ -0,0 +1,20 @@
+module ULICS
+
+using Crystallography: CrystalSystem
+using ..LinearElasticity: EngineeringStrain, minimal_npairs
+using Random: shuffle
+
+export ulics
+
+# See Yu, R., Zhu, J. & Ye, H. Q. Calculations of single-crystal elastic constants made simple. Comput Phys Commun 181, 671–675 (2010).
+const U₁ = EngineeringStrain(1:6) * 1e-3
+const U₂ = EngineeringStrain(-2, 1, 4, -3, 6, -5) * 1e-3
+const U₃ = EngineeringStrain(3, -5, -1, 6, 2, -4) * 1e-3
+const U₄ = EngineeringStrain(-4, -6, 5, 1, -3, 2) * 1e-3
+const U₅ = EngineeringStrain(5, 4, 6, -2, -1, -3) * 1e-3
+const U₆ = EngineeringStrain(-6, 3, -2, 5, -4, 1) * 1e-3
+const U = [U₁, U₂, U₃, U₄, U₅, U₆]
+
+ulics(system::CrystalSystem) = shuffle(U)[1:minimal_npairs(system)]
+
+end
