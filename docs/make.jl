7b24b772aeffe998785a88ccbdcd4e8298e77677
singularitti <singularitti@outlook.com>
2023-02-02 04:44:07 -0500

    Update docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -17,6 +17,16 @@ makedocs(;
     ),
     pages=[
         "Home" => "index.md",
+        "Manual" => [
+            "Installation Guide" => "installation.md",
+        ],
+        # "Public API" => "public.md",
+        "Developer Docs" => [
+            "Contributing" => "developers/contributing.md",
+            "Style Guide" => "developers/style-guide.md",
+            "Design Principles" => "developers/design-principles.md",
+        ],
+        "Troubleshooting" => "troubleshooting.md",
     ],
 )
 
