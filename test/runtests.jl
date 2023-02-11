e1dd90b492872579ad72ad9f9179dba7ff3732c2
singularitti <singularitti@outlook.com>
2023-02-10 20:29:46 -0500

    Remove test/invariants.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,6 @@ using Test
 
 @testset "LinearElasticity.jl" begin
     include("tensors.jl")
-    include("arithmetic.jl")
-    include("invariants.jl")
     include("misc.jl")
     include("solve.jl")
     include("ULICS.jl")
